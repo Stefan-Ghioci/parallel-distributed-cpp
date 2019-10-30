c5e663f9b0c945b67b50ca98a6652e851a408f44
Stefan Ghioci <ghioci.stefan@gmail.com>
2019-10-30 08:17:09 +0200

    Change output paths

--- arithmetic/paths.cpp
+++ arithmetic/paths.cpp
@@ -1,6 +1,6 @@
 ﻿#include "paths.h"
-std::string numbers_filename = "Debug/numbers.txt";
-std::string parallel_sum_filename = "Debug/parallel_sum.txt";
-std::string optimised_parallel_sum_filename = "Debug/optimised_parallel_sum.txt";
-std::string results_filename = "Debug/results.csv";
-std::string sequential_sum_filename = "Debug/sequential_sum.txt";
+std::string numbers_filename = "numbers.txt";
+std::string parallel_sum_filename = "parallel_sum.txt";
+std::string optimised_parallel_sum_filename = "optimised_parallel_sum.txt";
+std::string results_filename = "results.csv";
+std::string sequential_sum_filename = "sequential_sum.txt";
